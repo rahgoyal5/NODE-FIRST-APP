29c5e30628a01b2e2d3478e563b20fa3f00e28d2
Rahul Goyal <rahgoyal5@publicisgroupe.net>
2024-01-17 14:42:47 +0530

    added Pug, handlebars and ejs dynamic template engines

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,26 @@
 const express = require('express');
-const path = require('path');
-// const rootDir = require('../utils/path');
 const router = express.Router();
 
-router.use('/add-product', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+const products = [];
+
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formCSS: true,
+    prodcutCSS: true,
+    activeAddProduct: true
+  });
+});
+
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
 });
 
 router.get('/product', (req, res, next) => {
   res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,24 @@
 const express = require('express');
-const path = require('path');
 const router = express.Router();
+const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+  /**
+   * express by default is using pug as dynamic template engine as we
+   * mentioned in app.js
+   * dont need to give .pug extension as we have set pug as engine
+   * so it will only look for .pug files
+   */
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.lengh > 0,
+    activeShop: true,
+    productCSS: true,
+    layout: false
+  });
 });
 
 module.exports = router;
